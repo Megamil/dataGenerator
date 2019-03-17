5bf93337c45fc79d5440c6928c9dad5f68ba33b0
Eduardo dos santos <megamil3d@gmail.com>
2019-03-17 20:30:25 -0300

    Initial Commit

---
+++ dataGenerator/dataGenerator.h
@@ -0,0 +1,19 @@
+//
+//  dataGenerator.h
+//  dataGenerator
+//
+//  Created by Eduardo dos santos on 17/03/19.
+//  Copyright © 2019 Eduardo dos santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for dataGenerator.
+FOUNDATION_EXPORT double dataGeneratorVersionNumber;
+
+//! Project version string for dataGenerator.
+FOUNDATION_EXPORT const unsigned char dataGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dataGenerator/PublicHeader.h>
+
+
